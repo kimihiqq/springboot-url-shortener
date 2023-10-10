e50cabbf4536170ee816b986cbe269141125674a
kimihiqq <kimihiqq@gmail.com>
2023-10-10 15:43:39 +0900

    feat: ShortUrl Rest컨트롤러 추가

--- src/main/java/me/kimihiqq/springbooturlshortener/SpringbootUrlShortenerApplication.java
+++ src/main/java/me/kimihiqq/springbooturlshortener/SpringbootUrlShortenerApplication.java
@@ -9,5 +9,4 @@ public class SpringbootUrlShortenerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootUrlShortenerApplication.class, args);
 	}
-
 }
---
+++ src/test/java/me/kimihiqq/springbooturlshortener/SpringbootShortUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kimihiqq.springbooturlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShortUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
